9129c7ae4e27d5f197d0e972452dca80db2581eb
Alexandre Charoy <alexandre.charoy@gmail.com>
2020-01-02 13:16:22 +0100

    Added weather info

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,7 @@
   import Select from "svelte-select";
 
   let cities = [];
-  let selectedCity = "";
+  let selectedCity = undefined;
   let weather = undefined;
   let forecast = undefined;
 
@@ -33,29 +33,37 @@
   // Calls API: get forecast for selected city
   async function callApiGetForecast(city) {
     const response = await fetch(
-      `https://api.weatherbit.io/v2.0/forecast/daily?key=d61b050238b54f99ab1f0b90194387ea&city=${city}&days=5`
+      `https://api.weatherbit.io/v2.0/forecast/daily?key=d61b050238b54f99ab1f0b90194387ea&city=${city}&days=6`
     );
     return await response.json();
   }
 
   // City selection - triggers search
   async function handleSelect(selection) {
-    selectedCity = selection.detail.value;
+    selectedCity = selection.detail;
 
     // Get current weather data
-    const responseCurrentWeather = await callApiGetCurrentWeather(selectedCity);
+    const responseCurrentWeather = await callApiGetCurrentWeather(
+      selectedCity.value
+    );
     console.log(responseCurrentWeather.data[0]);
     weather = responseCurrentWeather.data[0];
 
     // Get forecast data
-    const responseForecast = await callApiGetForecast(selectedCity);
+    const responseForecast = await callApiGetForecast(selectedCity.value);
     console.log(responseForecast.data);
     forecast = responseForecast.data;
   }
 
   // Clear city input field
   function handleClear() {
-    selectedCity = "";
+    selectedCity = undefined;
+  }
+
+  // Round down a number to n decimal positions
+  function roundDown(number, decimals) {
+    decimals = decimals || 0;
+    return Math.floor(number * Math.pow(10, decimals)) / Math.pow(10, decimals);
   }
 
   // Load available cities from JSON on component mount
@@ -86,15 +94,21 @@
   </div>
   {#if weather != undefined}
   <div id="weather-container">
-    <img
-      src="../icons/{weather.weather.icon}.png"
-      alt="current weather icon"
-    /><br />
-    {weather.weather.description}
+    <div id="weather">
+      <img
+        src="../icons/{weather.weather.icon}.png"
+        alt="current weather icon"
+      />
+      <strong>{selectedCity.label}</strong><br />
+      <span>Weather: {weather.weather.description}</span><br />
+      <span>Temperature: {weather.temp}°C</span><br />
+      <span>Wind: {roundDown(weather.wind_spd)} m/s, {weather.wind_cdir}</span>
+    </div>
     <div id="forecast">
       {#if forecast != undefined} Forecast:<br />
       {#each forecast as day}
       <div class="forecast-day">
+        {day.valid_date}<br />
         <img
           src="../icons/{day.weather.icon}.png"
           alt="forecast weather icon"
@@ -111,17 +125,18 @@
   body {
     margin: 0px;
     text-align: center;
+    /*background-image: url("../bg.jpg");*/
   }
+
   main {
     margin: auto;
     padding: 1em;
     text-align: center;
     max-width: 500px;
-    /*background-image: url("../bg.jpg");*/
   }
 
   h1 {
-    color: #ff3e00;
+    color: #5555ff;
     text-transform: uppercase;
     font-size: 4em;
     font-weight: 100;
@@ -132,13 +147,24 @@
     margin-bottom: 20px;
   }
 
-  #forecast {
+  #weather-container {
     text-align: left;
+  }
+
+  #weather {
+    overflow: auto;
+  }
+
+  #weather > img {
+    float: left;
+  }
+
+  #forecast {
   }
 
   .forecast-day {
     float: left;
-    width: 92px;
+    width: 158px;
     margin-right: 8px;
     text-align: center;
   }
